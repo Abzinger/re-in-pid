513d9488be45a52bbb5cd954cd17e1f0734f7f6e
dojt <40855144+dojt@users.noreply.github.com>
2018-11-30 17:10:43 +0200

    Create intrinsic_reward.py

---
+++ intrinsic_reward.py
@@ -0,0 +1,16 @@
+from BROJA_2PID import pid
+
+def intrinsic_reward(n, piT, piX_T, piY_t):
+    p = dict()
+    for t in range(0,n):
+        for x in range(0,n):
+            for x in range(0,n):
+                if piT[T] > 0 and piX_T[x,t] > 0 and piY_T[y,t] > 0:
+                    p[ (t,x,y) ] = piX_T[x,t] * piY_T[y,t] / piT[t]
+            #
+        #
+    #
+    
+    res = pid(p)
+    return res['SI']
+#
