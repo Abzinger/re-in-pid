99f031b8b3e95552c9a50f7f6079e3c1e38b14eb
Abzinger <abdullah.makkeh@gmail.com>
2018-11-30 18:29:46 +0200

    update

--- intrinsic_reward.py
+++ intrinsic_reward.py
@@ -14,16 +14,14 @@ def intrinsic_reward(n, piT, piX_T, piY_T):
         for x in range(0,n):
             for y in range(0,n):
                 if piT[t] > 0 and piX_T[x,t] > 0 and piY_T[y,t] > 0:
-                    p_broja[ (t,x,y) ] = piX_T[x,t] * piY_T[y,t] / piT[t]
+                    p_broja[ (t,x,y) ] = piX_T[x,t] * piY_T[y,t] * piT[t]
                     pts.append(str(x)+str(y)+str(t))
-                    values.append(piX_T[x,t] * piY_T[y,t] / piT[t])
+                    values.append(piX_T[x,t] * piY_T[y,t] * piT[t])
             #^ x
         #^ y
     #^ z
-
     # Compute Shared Info using BROJA
     res_broja = BROJA_2PID.pid(p_broja)
-
     # Compute Shared Info using FL
     p_fl = dit.Distribution(pts,values)
     res_fl = dit.pid.ipm.PID_PM(p_fl)
