3cd670e4631198d8e3a74c5919fff0abf0cd7f95
Oriol Corcoll Andreu <ocorcoll@gmail.com>
2018-12-11 14:37:30 +0200

    Using global and local PID

--- re_in_pid/intrinsic_reward_fl.py
+++ re_in_pid/intrinsic_reward_fl.py
@@ -12,7 +12,8 @@ def intrinsic_reward_fl(piT, piX_T, piY_T):
     :param piT: numpy.ndarray (n) P(T)
     :param piX_T: numpy.ndarray (n, n) P(X|T)
     :param piY_T: numpy.ndarray (n, n) P(Y|T)
-    :return Tuple(float, float, float) si, ui_x, ui_y
+    :return tuple(tuple(float, float, float) tuple(numpy.ndarray (n, n, n), numpy.ndarray (n, n, n), numpy.ndarray (n, n, n)))
+        global and local si, ui_x, ui_y
     """
     n = piT.shape[0]
 
@@ -28,18 +29,22 @@ def intrinsic_reward_fl(piT, piX_T, piY_T):
         for y in range(0, n):
             piY[y] += piY_T[y, t] * piT[t]
 
-    si = 0
-    ui_x = 0
-    ui_y = 0
+    global_si = 0
+    global_ui_x = 0
+    global_ui_y = 0
+
+    local_si = np.zeros(shape=(n, n, n), dtype=float)
+    local_ui_x = np.zeros(shape=(n, n, n), dtype=float)
+    local_ui_y = np.zeros(shape=(n, n, n), dtype=float)
     for t in range(0, n):
         for x in range(0, n):
             for y in range(0, n):
-                local_si = min(-np.log2(piX[x]), -np.log2(piY[y])) - min(-np.log2(piX_T[x, t]), -np.log2(piY_T[y, t]))
-                local_ui_x = -np.log2(piX[x])+ log(piX_T[x,t]) - local_si
-                local_ui_y = -np.log2(piY[y])+ log(piY_T[y,t]) - local_si
+                local_si[t, x, y] = min(-np.log2(piX[x]), -np.log2(piY[y])) - min(-np.log2(piX_T[x, t]), -np.log2(piY_T[y, t]))
+                local_ui_x[t, x, y] = -np.log2(piX[x]) + np.log2(piX_T[x, t]) - local_si[t, x, y]
+                local_ui_y[t, x, y] = -np.log2(piY[y]) + np.log2(piY_T[y, t]) - local_si[t, x, y]
 
-                si += piX_T[x, t] * piY_T[y, t] * piT[t] * local_si
-                ui_x += piX_T[x, t] * piY_T[y, t] * piT[t] * local_ui_x
-                ui_y += piX_T[x, t] * piY_T[y, t] * piT[t] * local_ui_y
+                global_si += piX_T[x, t] * piY_T[y, t] * piT[t] * (local_si[t, x, y])
+                global_ui_x += piX_T[x, t] * piY_T[y, t] * piT[t] * (local_ui_x[t, x, y])
+                global_ui_y += piX_T[x, t] * piY_T[y, t] * piT[t] * (local_ui_y[t, x, y])
 
-    return si, ui_x, ui_y
+    return (global_si, global_ui_x, global_ui_y), (local_si, local_ui_x, local_ui_y)
